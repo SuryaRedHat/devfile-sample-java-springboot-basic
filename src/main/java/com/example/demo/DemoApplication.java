0ec31cdf8337452872677ed7c2b6966d6682dcb0
SuryaRedHat <106645005+SuryaRedHat@users.noreply.github.com>
2023-06-17 12:14:01 +0530

    Update DemoApplication.java
    Springup1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Surya says Hello World, I am running Spring application in Containers!";
     }
 
     public static void main(String[] args) {
